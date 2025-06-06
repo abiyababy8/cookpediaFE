b84ed902dcbced8669b6370a7ed254e04aab6d0a
abiyababy8 <abiyababy16@gmail.com>
2025-06-06 12:24:07 +0530

    added some code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  allRecipes: any = []
+  constructor(private api: ApiService) { }
+  ngOnInit() {
+    this.getAllRecipes()
+  }
+  getAllRecipes() {
+    this.api.getAllRecipesApi().subscribe((res: any) => {
+      console.log("All recipes in Home Page")
+      console.log(res)
+      this.allRecipes = res.slice(0, 6)
+    })
+  }
 }
